9fd27416c3d30e602c5e884d85a9e975c1bf5b8b
Alice Chen <alice.chen@hp.com>
2014-10-31 16:46:58 -0700

    Created pre-defined test subsets
    Created the following pre-defined test subsets which can be invoked
    by passing QUICK1, QUICK2, PART1, or PART2 to the --tests input
    parameter :
      QUICK1 - Current tests used by the phoenix_quick job. Takes
               approx. 15 minutes to run.
      QUICK2 - Tests selected based on whether it had failed before
               on elapsed time. Takes approx. 15 minutes to run.
      PART1  - First half of the phoenix test divided by time. Takes
               approx. 30 minutes to run.
      PART2  - Second half of the phoenix test divided by time. Takes
               approx. 30 minutes to run.
    
    Also fixed file to conform to Python's PEP8 Style Guide.
    
    Change-Id: I13f452511d821695b2f52b41a749237d4fc628da

--- phoenix_test.py
+++ phoenix_test.py
@@ -18,15 +18,56 @@
 #
 # @@@ END COPYRIGHT @@@
 
-import sys
-import subprocess
-import fcntl
-import select
 import errno
-import os
-import string
+import fcntl
 import inspect
 import optparse
+import os
+import select
+import string
+import subprocess
+import sys
+
+
+#----------------------------------------------------------------------------
+# Dictionary for the following pre-defined test groups :
+#    PART1 : First half of the phoenix test divided by time. Takes
+#            approx 30 min.
+#    PART2 : Second half of the phoenix test divided by time. Takes
+#            approx 30 min.
+#    QUICK1: Half of the phoenix tests selected by probability of
+#            failure and time (old set). Takes approx. 15 min to run.
+#    QUICK2: Half of the phoenix tests selected by probability of
+#            failure and time (new set). Takes approx. 15 min to run.
+#----------------------------------------------------------------------------
+TEST_SUBSET = {'PART1': ['AlterTableTest', 'ArithmeticQueryTest', 'BinaryRowKeyTest',
+                         'CoalesceFunctionTest', 'CompareDecimalToLongTest', 'CustomEntityDataTest',
+                         'DeleteRangeTest', 'DescColumnSortOrderTest', 'DistinctCountTest',
+                         'ExtendedQueryExecTest', 'FunkyNamesTest', 'IsNullTest', 'OrderByTest',
+                         'QueryExecTest', 'SaltedTableTest', 'SaltedTableUpsertSelectTest',
+                         'SaltedTableVarLengthRowKeyTest', 'SkipScanQueryTest',
+                         'UpsertSelectAutoCommitTest', 'UpsertSelectTest', 'UpsertValuesTest'],
+               'PART2': ['AutoCommitTest', 'CreateTableTest', 'ExecuteStatementsTest',
+                         'GroupByCaseTest', 'IndexTest', 'KeyOnlyTest', 'MultiCfQueryExecTest',
+                         'ProductMetricsTest', 'QueryExecWithoutSCNTest', 'QueryPlanTest',
+                         'ReadIsolationLevelTest', 'ServerExceptionTest', 'StatementHintsTest',
+                         'StddevTest', 'ToCharFunctionTest', 'ToNumberFunctionTest', 'TopNTest',
+                         'UpsertBigValuesTest', 'VariableLengthPKTest'],
+               'QUICK1': ['AlterTableTest', 'ArithmeticQueryTest', 'AutoCommitTest',
+                          'BinaryRowKeyTest', 'CoalesceFunctionTest', 'CompareDecimalToLongTest',
+                          'CreateTableTest', 'CustomEntityDataTest', 'DeleteRangeTest',
+                          'DistinctCountTest', 'ExecuteStatementsTest', 'ExtendedQueryExecTest',
+                          'FunkyNamesTest', 'GroupByCaseTest', 'IndexTest', 'IsNullTest',
+                          'KeyOnlyTest', 'MultiCfQueryExecTest', 'OrderByTest'],
+               'QUICK2': ['AlterTableTest', 'ArithmeticQueryTest', 'AutoCommitTest',
+                          'BinaryRowKeyTest', 'CompareDecimalToLongTest', 'CreateTableTest',
+                          'DeleteRangeTest', 'DistinctCountTest', 'ExecuteStatementsTest',
+                          'GroupByCaseTest', 'IndexTest', 'IsNullTest', 'KeyOnlyTest',
+                          'OrderByTest', 'QueryExecWithoutSCNTest', 'SaltedTableUpsertSelectTest',
+                          'StatementHintsTest', 'ToCharFunctionTest', 'ToNumberFunctionTest',
+                          'TopNTest', 'UpsertSelectAutoCommitTest']
+               }
+
 
 #----------------------------------------------------------------------------
 # A function (or a structure in C's sense) that contains all variables
@@ -50,6 +91,7 @@ def ArgList():
     _jdbc_type = None
     _tests = None
 
+
 #----------------------------------------------------------------------------
 # Tracking all of the global variables in here.
 #----------------------------------------------------------------------------
@@ -57,12 +99,14 @@ def gvars():
     my_ROOT = None
     my_EXPORT_CMD = None
 
+
 #----------------------------------------------------------------------------
 # Make a call to the shell and fetch the results back.
 #----------------------------------------------------------------------------
 def stdout_write(output):
     sys.stdout.write(output)
     sys.stdout.flush()
+
 
 def read_async(fd):
     try:
@@ -73,12 +117,14 @@ def read_async(fd):
         else:
             return ''
 
+
 def shell_call(cmd):
     # subprocess.call(cmd)
     ON_POSIX = 'posix' in sys.builtin_module_names
     hpipe = subprocess.Popen(cmd, stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        bufsize=1, close_fds=ON_POSIX, shell=True)
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             bufsize=1, close_fds=ON_POSIX, shell=True)
+
     # Change to non-blocking mode, so read returns even if there is no data.
     fcntl.fcntl(hpipe.stdout, fcntl.F_SETFL, os.O_NONBLOCK)
     fcntl.fcntl(hpipe.stderr, fcntl.F_SETFL, os.O_NONBLOCK)
@@ -92,7 +138,7 @@ def shell_call(cmd):
         # Try reading some data from each
         output_stdout = read_async(hpipe.stdout)
         output_stderr = read_async(hpipe.stderr)
-    
+
         if output_stdout:
             stdout_write(output_stdout)
             total_output_stdout += output_stdout
@@ -101,8 +147,9 @@ def shell_call(cmd):
             total_output_stderr += output_stderr
 
         rc = hpipe.poll()
-        if rc != None:
+        if rc is not None:
             return total_output_stdout + total_output_stderr
+
 
 #----------------------------------------------------------------------------
 # Find the token after s2 in s1
@@ -112,9 +159,10 @@ def get_token_after_string(s1, s2, extra_split_chars=None):
         return None
     else:
         token = s1[s1.find(s2)+len(s2):].split()[0]
-        if extra_split_chars != None:
+        if extra_split_chars is not None:
             token = token.split(extra_split_chars)[0]
         return token
+
 
 #----------------------------------------------------------------------------
 # Find the sub string after s2 in s1
@@ -125,11 +173,12 @@ def get_substr_after_string(s1, s2):
     else:
         return s1[s1.find(s2)+len(s2):]
 
+
 #----------------------------------------------------------------------------
 # Generate pom.xml
 #----------------------------------------------------------------------------
 def generate_pom_xml(targettype, jdbccp):
-    fd1= open(os.path.join(gvars.my_ROOT, 'pom.xml.template'), 'r')
+    fd1 = open(os.path.join(gvars.my_ROOT, 'pom.xml.template'), 'r')
     fd2 = open(os.path.join(gvars.my_ROOT, 'pom.xml'), 'w')
 
     for line in fd1:
@@ -138,6 +187,7 @@ def generate_pom_xml(targettype, jdbccp):
 
     fd1.close()
     fd2.close()
+
 
 #----------------------------------------------------------------------------
 # Generate propfile
@@ -157,6 +207,7 @@ def generate_t4_propfile(propfile, target, user, pw, role, dsn, targettype):
 
     fd.close()
 
+
 def generate_t2_propfile(propfile, targettype):
     fd = open(propfile, 'w')
     fd.write('url=jdbc:sql:\n')
@@ -172,21 +223,22 @@ def generate_t2_propfile(propfile, targettype):
 
     fd.close()
 
+
 #----------------------------------------------------------------------------
 # Parse the argument list for main
 #----------------------------------------------------------------------------
 def prog_parse_args():
-    rec = inspect.stack()[1] # 0 reprents this line,
-                             # 1 reprents line at caller,
-                             # 2 reprents line at caller's caller,
-                             # ... and so on ...
+    rec = inspect.stack()[1]  # 0 reprents this line,
+                              # 1 reprents line at caller,
+                              # 2 reprents line at caller's caller,
+                              # ... and so on ...
     frame = rec[0]
     info = inspect.getframeinfo(frame)
     gvars.my_ROOT = os.path.dirname(os.path.abspath(info.filename))
 
     DEFAULT_JDBC_CLASSPATH = '${project.basedir}/lib/hp/tr/jdbcT4.jar'
     DEFAULT_PROP_FILE = os.path.join(gvars.my_ROOT, 'jdbcprop')
-    
+
     # alas, the more powerful argparse module only exists in >= 2.7 and >= 3.2,
     # use optparse instead.
     option_list = [
@@ -195,62 +247,73 @@ def prog_parse_args():
         # we do not use short options, hence the first ''.
         # required args
         optparse.make_option('', '--target', action='store', type='string',
-          dest='target',
-          help='target \'<IP>:<port>\', required argument with no default. This is still needed for jdbc loader even when using sqlci.'),
+                             dest='target',
+                             help="target '<IP>:<port>', required argument with no default. " +
+                                  "This is still needed for jdbc loader even when using sqlci."),
         optparse.make_option('', '--user', action='store', type='string',
-          dest='user',
-          help='user id for the target, required argument with no default. It can be any non-empty string if using sqlci.'),
+                             dest='user',
+                             help='user id for the target, required argument with no default. ' +
+                                  'It can be any non-empty string if using sqlci.'),
         optparse.make_option('', '--pw', action='store', type='string',
-          dest='pw',
-          help='password for the target, required argument with no default. It can be any non-empty string if using sqlci.'),
+                             dest='pw',
+                             help='password for the target, required argument with no default. ' +
+                                  'It can be any non-empty string if using sqlci.'),
 
         # optional args
         optparse.make_option('', '--role', action='store', type='string',
-          dest='role', default='',
-          help='role for the target, defaulted to \'\''),
+                             dest='role', default='',
+                             help="role for the target, defaulted to ''"),
         optparse.make_option('', '--dsn', action='store', type='string',
-          dest='dsn', default='TDM_Default_DataSource',
-          help='data source for the target, defaulted to \'TDM_Default_DataSource\''),
+                             dest='dsn', default='TDM_Default_DataSource',
+                             help="data source for the target, defaulted to " +
+                                  "'TDM_Default_DataSource'"),
         optparse.make_option('', '--javahome', action='store', type='string',
-          dest='javahome', default='/usr',
-          help='java program (version 1.7 required) location, defaulted to \'/usr\''),
+                             dest='javahome', default='/usr',
+                             help="java program (version 1.7 required) location, " +
+                                  "defaulted to '/usr'"),
         optparse.make_option('', '--jdbccp', action='store', type='string',
-          dest='jdbccp', default=DEFAULT_JDBC_CLASSPATH,
-          help='jdbc classpath, defaulted to \'${project.basedir}/lib/hp/tr/jdbcT4.jar\', <test_root> is where this program is.'),
+                             dest='jdbccp', default=DEFAULT_JDBC_CLASSPATH,
+                             help="jdbc classpath, defaulted to " +
+                                  "'${project.basedir}/lib/hp/tr/jdbcT4.jar', \t\t "
+                                  "<test_root> is where this program is."),
         optparse.make_option('', '--propfile', action='store', type='string',
-          dest='propfile', default=DEFAULT_PROP_FILE,
-          help='property file, defaulted to automatically generated \'<test root>/jdbcprop\', <test root> is where this program is.'),
+                             dest='propfile', default=DEFAULT_PROP_FILE,
+                             help="property file, defaulted to automatically generated " +
+                                  "'<test root>/jdbcprop', <test root> is where this program is."),
         optparse.make_option('', '--targettype', action='store', type='string',
-          dest='targettype', default='TR',
-          help='target type, SQ for Seaquest, TR for TRAFODION, defaulted to TR'),
+                             dest='targettype', default='TR',
+                             help='target type, SQ for Seaquest, TR for TRAFODION, ' +
+                                  'defaulted to TR'),
         optparse.make_option('', '--jdbctype', action='store', type='string',
-          dest='jdbctype', default='T4',
-          help='jdbctype, defaulted to T4'
-),
+                             dest='jdbctype', default='T4',
+                             help='jdbctype, defaulted to T4'),
         optparse.make_option('', '--export1', action='store', type='string',
-          dest='exportstr1', default='NONE',
-          help='any export string, defaulted to NONE'
-),
+                             dest='exportstr1', default='NONE',
+                             help='any export string, defaulted to NONE'),
         optparse.make_option('', '--export2', action='store', type='string',
-          dest='exportstr2', default='NONE',
-          help='any export string, defaulted to NONE'
-),
+                             dest='exportstr2', default='NONE',
+                             help='any export string, defaulted to NONE'),
         optparse.make_option('', '--export3', action='store', type='string',
-          dest='exportstr3', default='NONE',
-          help='any export string, defaulted to NONE'
-),
+                             dest='exportstr3', default='NONE',
+                             help='any export string, defaulted to NONE'),
         optparse.make_option('', '--export4', action='store', type='string',
-          dest='exportstr4', default='NONE',
-          help='any export string, defaulted to NONE'
-),
+                             dest='exportstr4', default='NONE',
+                             help='any export string, defaulted to NONE'),
         optparse.make_option('', '--export5', action='store', type='string',
-          dest='exportstr5', default='NONE',
-          help='any export string, defaulted to NONE'
-),
+                             dest='exportstr5', default='NONE',
+                             help='any export string, defaulted to NONE'),
         optparse.make_option('', '--tests', action='store', type='string',
-          dest='tests', default='.*',
-          help='specify a subset of tests to run, for example: --tests=AlterTableTest or --tests=AlterTableTest,ArithmeticQueryTest.  Multiple tests can be specified as a string with each test sperated by \',\' without space characters.  When this option is omitted, the default is ALL tests.')
+                             dest='tests', default='.*',
+                             help="specify a subset of tests to run. This can either be a string " +
+                                  "with each test sperated by ',' without space characters OR " +
+                                  "one of the following pre-defined subsets: PART1, PART2, " +
+                                  "QUICK1, or QUICK2. If this option is omitted, the default is " +
+                                  "ALL tests. \t\t\t\t For example: \t\t\t\t\t\t\t\t\t " +
+                                  "--tests=AlterTableTest \t\t\t\t " +
+                                  "--tests=AlterTableTest,ArithmeticQueryTest \t\t" +
+                                  "--tests=QUICK1 \t\t\t\t\t\t\t\t")
     ]
+
     usage = 'usage: %prog [-h|--help|<options>]'
     parser = optparse.OptionParser(usage=usage, option_list=option_list)
     # OptionParser gets the options out, whatever is not preceeded by
@@ -264,18 +327,18 @@ def prog_parse_args():
 
     if options.targettype != 'SQ' and options.targettype != 'TR':
         parser.error('Invalid --targettype.  Only SQ aor TR is supported: ' +
-                     options.targettype);
-    
+                     options.targettype)
+
     if options.jdbctype != 'T2' and options.jdbctype != 'T4':
         parser.error('Invalid --jdbctype.  Only T2 or T4 is supported: ' +
-                     options.jdbctype);
+                     options.jdbctype)
 
     # check for the required args for certain conditions
     if options.jdbctype == 'T4':
         not_found = []
         T4_required_args = ['target', 'user', 'pw']
         for r in T4_required_args:
-            if options.__dict__[r] == None:
+            if options.__dict__[r] is None:
                 not_found.append('--' + r)
         if not_found:
             parser.error('Required option(s) not found: ' + str(not_found))
@@ -285,7 +348,8 @@ def prog_parse_args():
         if options.jdbctype == 'T2':
             generate_t2_propfile(options.propfile, options.targettype)
         elif options.jdbctype == 'T4':
-            generate_t4_propfile(options.propfile, options.target, options.user, options.pw, options.role, options.dsn, options.targettype)
+            generate_t4_propfile(options.propfile, options.target, options.user, options.pw,
+                                 options.role, options.dsn, options.targettype)
 
     ArgList._target = options.target
     ArgList._user = options.user
@@ -304,7 +368,14 @@ def prog_parse_args():
     ArgList._export_str3 = options.exportstr3
     ArgList._export_str4 = options.exportstr4
     ArgList._export_str5 = options.exportstr5
-    ArgList._tests = options.tests
+
+    # check if tests parameter was passed in a pre-defined test group
+    if options.tests.upper() in ['PART1', 'PART2', 'QUICK1', 'QUICK2']:
+        # tests parameter was passed a pre-defined test group so
+        # generate list of tests seperated by a comma
+        ArgList._tests = ','.join(map(str, TEST_SUBSET[options.tests.upper()]))
+    else:
+        ArgList._tests = options.tests
 
     # Generate the pom.xml file from the template according to target type
     generate_pom_xml(ArgList._target_type, ArgList._jdbc_classpath)
@@ -329,7 +400,7 @@ def prog_parse_args():
     else:
         print 'tests to be run:        ALL tests'
     print
-   
+
     sys.stdout.flush()
 
     gvars.my_EXPORT_CMD = 'export PATH=' + ArgList._javahome + '/bin:$PATH'
@@ -352,7 +423,7 @@ def prog_parse_args():
         gvars.my_EXPORT_CMD += 'export ' + ArgList._export_str4
     if ArgList._export_str5 != 'NONE':
         if gvars.my_EXPORT_CMD != '':
-           gvars.my_EXPORT_CMD += ';'
+            gvars.my_EXPORT_CMD += ';'
         gvars.my_EXPORT_CMD += 'export ' + ArgList._export_str5
 
 
@@ -370,15 +441,15 @@ output = shell_call(gvars.my_EXPORT_CMD + ';mvn test -Dtest=' + ArgList._tests)
 
 lines = output.split('\n')
 to_parse = False
-num_tests_run=0
-num_tests_failed=0
-num_tests_errors=0
-num_tests_skipped=0
+num_tests_run = 0
+num_tests_failed = 0
+num_tests_errors = 0
+num_tests_skipped = 0
 for ln in lines:
     if ln.startswith('Results '):
         to_parse = True
         continue
- 
+
     if to_parse:
         if ln.startswith('Tests run: '):
             num_tests_run = int(get_token_after_string(ln, 'Tests run:', ','))
@@ -392,4 +463,3 @@ sumfile_output += ('Total number of failures: ' + str(num_tests_failed) + '\n')
 sumfile_output += ('Total number of errors: ' + str(num_tests_errors) + '\n')
 sumfile_output += ('Total number of tests skipped: ' + str(num_tests_skipped) + '\n')
 stdout_write(sumfile_output)
-
