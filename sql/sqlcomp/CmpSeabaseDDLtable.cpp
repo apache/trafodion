3af42eaadfddc18aec3cb12715d8262b102b4d80
Prashanth Vasudev <vasudev.prashanth@hp.com>
2015-01-15 18:30:52 +0000

    Fix for Lp 1395233. Complete details in LP.
    Change-Id: Ib38a3e8ea016ddc183623aadf4821a64ef202fd9

--- sql/sqlcomp/CmpSeabaseDDLtable.cpp
+++ sql/sqlcomp/CmpSeabaseDDLtable.cpp
@@ -6833,6 +6833,7 @@ desc_struct * CmpSeabaseDDL::getSeabaseUserTableDesc(const NAString &catName,
   //Make sure to restore the CQDs after this query including error paths.            
   cliInterface.holdAndSetCQD("MERGE_JOINS", "OFF");
   cliInterface.holdAndSetCQD("HASH_JOINS", "OFF");
+  cliInterface.holdAndSetCQD("OPTIMIZATION_LEVEL", "3");
   
   Queue * indexInfoQueue = NULL;
   cliRC = cliInterface.fetchAllRows(indexInfoQueue, query, 0, FALSE, FALSE, TRUE);
@@ -6848,6 +6849,7 @@ desc_struct * CmpSeabaseDDL::getSeabaseUserTableDesc(const NAString &catName,
   //restore CQDs.
   cliInterface.restoreCQD("MERGE_JOINS");
   cliInterface.restoreCQD("HASH_JOINS");
+  cliInterface.restoreCQD("OPTIMIZATION_LEVEL");
   
   if (cliRC < 0)
     return NULL;
