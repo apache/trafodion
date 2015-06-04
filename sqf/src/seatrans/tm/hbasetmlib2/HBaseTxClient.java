0e4dd4c61000bc6f1612f2499ee32e58157a58ed
Oliver Bucaojit <oliver.bucaojit@hp.com>
2015-06-04 19:53:28 +0000

    Reducing recovery thread sleep delay to 1 second
    Recovery thread will check the ZK bulletin board
    every second now instead of 10 seconds.  Moved the config
    check to be done only on constructor instead of every
    iteration.
    
    Configuration value is: TMRECOV_SLEEP
    Value set in etc/ms.env
    Default value is 1000
    Units for TMRECOV_SLEEP is milliseconds
    i.e. 'TMRECOV_SLEEP=500' will set the sleep delay
                             to 500 ms
    
    Change-Id: Iffc18060b1d831efda40b2377eb40438c87c0e43

--- sqf/src/seatrans/tm/hbasetmlib2/HBaseTxClient.java
+++ sqf/src/seatrans/tm/hbasetmlib2/HBaseTxClient.java
@@ -871,7 +871,9 @@ public class HBaseTxClient {
       * Thread to gather recovery information for regions that need to be recovered
       */
      private static class RecoveryThread extends Thread{
-             final int SLEEP_DELAY = 10000; // Initially set to run every 10sec
+             final int SLEEP_DELAY = 1000; // Initially set to run every 1sec
+             private int sleepTimeInt = 0;
+             private boolean skipSleep = false;
              private TmAuditTlog audit;
              private HBaseTmZK zookeeper;
              private TransactionManager txnManager;
@@ -913,6 +915,13 @@ public class HBaseTxClient {
                           this.txnManager = txnManager;
                           this.inDoubtList = new HashSet<Long> ();
                           this.tmID = zookeeper.getTMID();
+
+                          String sleepTime = System.getenv("TMRECOV_SLEEP");
+                          if (sleepTime != null) {
+                                this.sleepTimeInt = Integer.parseInt(sleepTime);
+                                if(LOG.isDebugEnabled()) LOG.debug("Recovery thread sleep set to: " +
+                                                                   this.sleepTimeInt + "ms");
+                          }
              }
 
              public void stopThread() {
@@ -958,13 +967,10 @@ public class HBaseTxClient {
 
             @Override
              public void run() {
-                int sleepTimeInt = 0;
-                String sleepTime = System.getenv("TMRECOV_SLEEP");
-                if (sleepTime != null)
-                    sleepTimeInt = Integer.parseInt(sleepTime);
 
                 while (this.continueThread) {
                     try {
+                        skipSleep = false;
                         Map<String, byte[]> regions = null;
                         Map<Long, TransactionState> transactionStates =
                                 new HashMap<Long, TransactionState>();
@@ -981,6 +987,7 @@ public class HBaseTxClient {
                         }
 
                         if(regions != null) {
+                            skipSleep = true;
                             recoveryIterations++;
 
                             if (LOG.isDebugEnabled()) LOG.debug("TRAF RCOV THREAD: in-doubt region size " + regions.size());
@@ -1183,10 +1190,12 @@ public class HBaseTxClient {
                         }
                         try {
                             if(continueThread) {
-                                if (sleepTimeInt > 0)
-                                    Thread.sleep(sleepTimeInt);
-                                else
-                                    Thread.sleep(SLEEP_DELAY);
+                                if(!skipSleep) {
+                                    if (sleepTimeInt > 0)
+                                        Thread.sleep(sleepTimeInt);
+                                    else
+                                        Thread.sleep(SLEEP_DELAY);
+                                }
                             }
                             retryCount = 0;
                         } catch (Exception e) {
