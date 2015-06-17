trafodion/core

WRONG QUERY_STATUS/QUERY_SUB_STATUS for canceled queries
Change-Id: I12fb063343f7095fddd6abedd500e2683a64a911

conn/odbc/src/odbc/Common/QSData.h
@@ -270,6 +270,7 @@ typedef enum _QUERY_STATE
 	QUERY_COMPLETED_NDCS_CLOSE			= 0x081A,
 	QUERY_COMPLETED_NDCS_PREPARE		= 0x081B,
 	QUERY_COMPLETED_NDCS_WMS_ERROR		= 0x081C,
+	QUERY_COMPLETED_BY_ADMIN_1          = 0x081D,
 //
 	QUERY_REJECTED						= 0x1000,
 	QUERY_REJECTED_BY_ADMIN				= 0x1001,
@@ -2918,7 +2919,8 @@ const string FMT_BY_ADMIN					= "WMS_ADMIN";
 const string FMT_HOLD_TIMEOUT				= "WMS_HOLD_TIMEOUT";
 const string FMT_EXEC_TIMEOUT				= "WMS_EXEC_TIMEOUT"; 
 const string FMT_CANCELLED_BY_ADMIN			= "WMS_QUERY_CANCELLED"; 
-const string FMT_CANCELLED_BY_CLIENT		= "CLIENT_QUERY_CANCELLED";
+const string FMT_CANCELLED_BY_ADMIN_1			= "QUERY_CANCELLED"; 
+const string FMT_CANCELLED_BY_CLIENT			= "CLIENT_QUERY_CANCELLED";
 const string FMT_CANCEL_IN_PROGRESS			= "CANCEL_IN_PROGRESS";
 const string FMT_CANCEL_FAILED				= "CANCEL_FAILED";
 const string FMT_CANCEL_FAILED_8026			= "CANCEL_FAILED-8026";

conn/odbc/src/odbc/nsksrvr/SrvrConnect.cpp
@@ -9423,7 +9423,21 @@ short qrysrvc_ExecuteFinished(
 
 	// Update the query status
 	if (pSrvrStmt->m_bqueryFinish)
+	{
 		pSrvrStmt->m_state = QUERY_COMPLETED;
+		if (pSrvrStmt->sqlWarningOrError)
+		{
+			Int32 sqlError = *(Int32 *)(pSrvrStmt->sqlWarningOrError+8);
+			if (sqlError == -8007)
+			{
+				pSrvrStmt->m_state = QUERY_COMPLETED_BY_ADMIN_1;
+			}
+		}
+		else if (STMTSTAT_CLOSE == pSrvrStmt->inState)
+		{
+			pSrvrStmt->m_state = QUERY_COMPLETED_BY_CLIENT;
+		}
+	}
 
 	if (resStatStatement != NULL)
 	{

apache/trafodion

conn/odbc/src/odbc/Common/QSData.h
@@ -270,6 +270,7 @@ typedef enum _QUERY_STATE
 	QUERY_COMPLETED_NDCS_CLOSE			= 0x081A,
 	QUERY_COMPLETED_NDCS_PREPARE		= 0x081B,
 	QUERY_COMPLETED_NDCS_WMS_ERROR		= 0x081C,
+	QUERY_COMPLETED_BY_ADMIN_SERVER     = 0x081D,
 //
 	QUERY_REJECTED						= 0x1000,
 	QUERY_REJECTED_BY_ADMIN				= 0x1001,
@@ -2918,7 +2919,8 @@ const string FMT_BY_ADMIN					= "WMS_ADMIN";
 const string FMT_HOLD_TIMEOUT				= "WMS_HOLD_TIMEOUT";
 const string FMT_EXEC_TIMEOUT				= "WMS_EXEC_TIMEOUT"; 
 const string FMT_CANCELLED_BY_ADMIN			= "WMS_QUERY_CANCELLED"; 
-const string FMT_CANCELLED_BY_CLIENT		= "CLIENT_QUERY_CANCELLED";
+const string FMT_CANCELLED_BY_ADMIN_SERVER		= "QUERY_CANCELLED";
+const string FMT_CANCELLED_BY_CLIENT			= "CLIENT_QUERY_CANCELLED";
 const string FMT_CANCEL_IN_PROGRESS			= "CANCEL_IN_PROGRESS";
 const string FMT_CANCEL_FAILED				= "CANCEL_FAILED";
 const string FMT_CANCEL_FAILED_8026			= "CANCEL_FAILED-8026";

conn/odbc/src/odbc/nsksrvr/SrvrConnect.cpp
@@ -9423,7 +9423,21 @@ short qrysrvc_ExecuteFinished(
 
 	// Update the query status
 	if (pSrvrStmt->m_bqueryFinish)
+	{
 		pSrvrStmt->m_state = QUERY_COMPLETED;
+		if (pSrvrStmt->sqlWarningOrError)
+		{
+			Int32 sqlError = *(Int32 *)(pSrvrStmt->sqlWarningOrError+8);
+			if (sqlError == -8007)
+			{
+				pSrvrStmt->m_state = QUERY_COMPLETED_BY_ADMIN_SERVER;
+			}
+		}
+		else if (STMTSTAT_CLOSE == pSrvrStmt->inState)
+		{
+			pSrvrStmt->m_state = QUERY_COMPLETED_BY_CLIENT;
+		}
+	}
 
 	if (resStatStatement != NULL)
 	{