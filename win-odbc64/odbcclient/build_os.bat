f9bc12d9327dca79652a1335770dfe63a504d7fb
Weiqing Xu <weiqing.xu@esgyn.cn>
2015-10-12 20:32:53 +0800

    [TRAFODION-1519]Use free tool to build windows ODBC
    1. Use Visual Studio Community 2013 instead of Visual Studio 2010.
    Visual Studio Community 2013 is free.
    2. Use open source tool inno setup to package windows ODBC Driver.
    3. cleanup unused files

--- win-odbc64/odbcclient/build_os.bat
+++ win-odbc64/odbcclient/build_os.bat
@@ -20,24 +20,31 @@ REM under the License.
 REM
 REM @@@ END COPYRIGHT @@@
 
-REM set this to openssl binary directory.
-set OPENSSL_PATH=E:\OpenSSL101e_bin
-REM set ODBCCP32_32_PATH="E:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\Lib"
-REM set this to vs2008 64bit odbccp32.lib directory.
-set ODBCCP32_64_PATH="E:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\Lib\amd64"
-set MSBUILD_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319
-REM set this to top directory include everything for build
-set BUILDDIR=E:
-REM set this to where git bash was installed.(from git install)
-set GIT_BASH="C:\Program Files (x86)\Git\bin\sh.exe"
+@set INNO_SETUP_PATH="C:\Program Files (x86)\Inno Setup 5"
+@set PATH=%INNO_SETUP_PATH%;%PATH%
 
-set ZLIB_SRC=%BUILDDIR%\zlib_official
-set ZLIB_SRC_GIT=%ZLIB_SRC:\=/%
+set MSBUILD_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+REM get the build directory automatically, if failed please set this to top directory include everything for build
+CD ..\..
+SET BUILDDIR=%CD%
+CD win-odbc64\odbcclient
+
+REM set this to zlib header files directory
+set ZLIB_INCLUDE_PATH=C:\zlib\include
+REM set this to zlib library files directory
+set ZLIB_LIB_PATH=C:\zlib\lib
+
+REM set this to openssl header files directory
+set OPENSSL_INCLUDE_PATH=C:\openssl-1.0.1e\include
+REM set this to openssl library files directory
+set OPENSSL_LIB_PATH=C:\openssl-1.0.1e\lib
+
+set PACKDIR=C:\Build\winodbc64
 
 set SRCDIR=%BUILDDIR%\win-odbc64
-set LIBDIR=%BUILDDIR%\lib 
+set LIBDIR=%BUILDDIR%\lib
 set PATH=%MSBUILD_PATH%\;%PATH%
-set DATE=
+
 set ALL_SUCCESS=0
 
 echo=
@@ -45,28 +52,6 @@ echo ===============================
 echo     BUILD WIN64 RELEASE
 echo ===============================
 echo=
-
-echo Building zlib - Win64 Release...
-if not exist %ZLIB_SRC% (
-echo %GIT_BASH%
-%GIT_BASH% --login -c "git clone https://github.com/madler/zlib/ %ZLIB_SRC_GIT%"
-)
-
-cd %ZLIB_SRC%\contrib\vstudio\vc10
-msbuild.exe /t:rebuild zlibstat.vcxproj /p:Platform=x64 /p:Configuration=Release
-cd %ZLIB_SRC%
-
-if not exist %SRCDIR%\dependencies\zlib\lib\x64\Release (
-mkdir %SRCDIR%\dependencies\zlib\lib\x64\Release
-)
-if not exist %SRCDIR%\dependencies\zlib\include (
-mkdir %SRCDIR%\dependencies\zlib\include
-)
-copy /Y %ZLIB_SRC%\contrib\vstudio\vc10\x64\ZlibStatRelease\zlibstat.lib %SRCDIR%\dependencies\zlib\lib\x64\Release
-copy /Y %ZLIB_SRC%\zlib.h %SRCDIR%\dependencies\zlib\include
-copy /Y %ZLIB_SRC%\zconf.h %SRCDIR%\dependencies\zlib\include
-cd %SRCDIR%
-rmdir /S /Q %ZLIB_SRC%
 
 echo Building Drvr35Msg - Win64 Release...
 cd %SRCDIR%\odbcclient\Drvr35Msg
@@ -82,22 +67,22 @@ if %BUILD_STATUS%==0 (
 
 echo Building Drvr35 - Win64 Release...
 cd %SRCDIR%\odbcclient\Drvr35
-msbuild.exe /t:rebuild Drvr35_os.vcxproj /p:Platform=x64 /p:Configuration=Release /p:OpenSSLIncludeDir=%OPENSSL_PATH%\include /p:OpenSSLLibraryDir=%OPENSSL_PATH%\lib\VC\static 
+msbuild.exe /t:rebuild Drvr35_os.vcxproj /p:Platform=x64 /p:Configuration=Release /p:OpenSSLIncludeDir=%OPENSSL_INCLUDE_PATH% /p:OpenSSLLibraryDir=%OPENSSL_LIB_PATH% /p:ZlibIncludeDir=%ZLIB_INCLUDE_PATH% /p:ZlibLibDir=%ZLIB_LIB_PATH%
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build Drvr35 success
-) else ( 
+) else (
 	echo Build Drvr35 failed
 	goto Exit
 )
-  
+
 echo Building Drvr35Adm - Win64 Release...
 cd %SRCDIR%\odbcclient\Drvr35Adm
 msbuild.exe /t:rebuild Drvr35Adm_os.vcxproj /p:Platform=x64 /p:Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build Drvr35Adm success
-) else ( 
+) else (
 	echo Build Drvr35Adm failed
 	goto Exit
 )
@@ -108,77 +93,71 @@ REM msbuild.exe /t:rebuild Drvr35Trace_os.vcxproj /p:Platform=x64 /p:Configurati
 REM set BUILD_STATUS=%ERRORLEVEL%
 REM if %BUILD_STATUS%==0 (
 REM	echo Build Drvr35Trace success
-REM ) else ( 
+REM ) else (
 REM	echo Build Drvr35Trace failed
 REM	goto Exit
 REM )
 
 echo Building TCPIPV4 - Win64 Release...
 cd %SRCDIR%\odbcclient\Drvr35\TCPIPV4
-msbuild.exe /t:rebuild TCPIPV4_os.vcxproj /p:Platform=x64 /p:Configuration=Release
+msbuild.exe /t:rebuild TCPIPV4_os.vcxproj /p:Platform=x64 /p:Configuration=Release /p:ZlibIncludeDir=%ZLIB_INCLUDE_PATH% /p:ZlibLibDir=%ZLIB_LIB_PATH%
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build TCPIPV4 success
-) else ( 
+) else (
 	echo Build TCPIPV4 failed
 	goto Exit
 )
 
 echo Building TCPIPV6 - Win64 Release...
 cd %SRCDIR%\odbcclient\Drvr35\TCPIPV6
-msbuild.exe /t:rebuild TCPIPV6_os.vcxproj /p:Platform=x64 /p:Configuration=Release
+msbuild.exe /t:rebuild TCPIPV6_os.vcxproj /p:Platform=x64 /p:Configuration=Release /p:ZlibIncludeDir=%ZLIB_INCLUDE_PATH% /p:ZlibLibDir=%ZLIB_LIB_PATH%
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build TCPIPV6 success
-) else ( 
+) else (
 	echo Build TCPIPV6 failed
 	goto Exit
 )
 
 echo Building TranslationDll - Win64 Release...
 cd %SRCDIR%\odbcclient\TranslationDll
-msbuild.exe /t:rebuild TranslationDll_os.vcxproj /p:Platform=x64 /p:Configuration=Release	
+msbuild.exe /t:rebuild TranslationDll_os.vcxproj /p:Platform=x64 /p:Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build TranslationDll success
-) else ( 
+) else (
 	echo Build TranslationDll failed
 	goto Exit
 )
 
 echo Building Drvr35Res - Win64 Release...
-cd %SRCDIR%\odbcclient\Drvr35Res 
-msbuild.exe /t:rebuild Drvr35Res_os.vcxproj /p:Platform=x64 /p:Configuration=Release	
+cd %SRCDIR%\odbcclient\Drvr35Res
+msbuild.exe /t:rebuild Drvr35Res_os.vcxproj /p:Platform=x64 /p:Configuration=Release
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
 	echo Build Drvr35Res success
-) else ( 
+) else (
 	echo Build Drvr35Res failed
 	goto Exit
 )
-   
-echo Building SetCertificateDirReg InstallHelper - Win64 Release...
-cd %SRCDIR%\Install\SetCertificateDirReg\SetCertificateDirReg
-msbuild.exe /t:rebuild SetCertificateDirReg_os.vcxproj /p:Platform=x64 /p:Configuration=Release /p:Odbccp32lib64Dir=%ODBCCP32_64_PATH% 
-set BUILD_STATUS=%ERRORLEVEL%
-if %BUILD_STATUS%==0 (
-	echo Build SetCertificateDirReg success
-) else ( 
-	echo Build SetCertificateDirReg failed
-	goto Exit
-)
- 
-echo Building UpdateDSN InstallHelper - Win64 Release...
-cd %SRCDIR%\Install\UpdateDSN\UpdateDSN
-msbuild.exe /t:rebuild UpdateDSN_os.vcxproj /p:Platform=x64 /p:Configuration=Release
-set BUILD_STATUS=%ERRORLEVEL%
-if %BUILD_STATUS%==0 (
-	echo Build UpdateDSN success
-) else ( 
-	echo Build UpdateDSN failed
-	goto Exit
-)
+
 set ALL_SUCCESS=1
+
+cd %SRCDIR%\odbcclient
+
+if not exist C:\Build\winodbc64\inc (
+	mkdir C:\Build\winodbc64\inc
+)
+copy /Y %BUILDDIR%\win-odbc64\odbcclient\inc\trafsqlext.h C:\Build\winodbc64\inc
+if not exist C:\Build\winodbc64\lib\x64\Release (
+	mkdir C:\Build\winodbc64\lib\x64\Release
+)
+copy /Y %BUILDDIR%\lib\x64\Release C:\Build\winodbc64\lib\x64\Release
+
+ISCC.exe /Q %BUILDDIR%\win-odbc64\Install\win64_installer\installer.iss
+move /Y %BUILDDIR%\win-odbc64\Install\win64_installer\Output\TFODBC64-1.2.0.exe %PACKDIR%
+@echo on
 
 :Exit
 if %ALL_SUCCESS%==1 (
@@ -188,5 +167,3 @@ if %ALL_SUCCESS%==1 (
 	echo ========================================
 	echo=
 )
-cd %SRCDIR%\odbcclient
-@echo on
