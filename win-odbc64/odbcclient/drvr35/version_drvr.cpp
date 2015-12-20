a1c3336fff35ab15596ff4936b9ae5fa2dea39fb
Gunnar Tapper <gtapper@esgyn.local>
2015-12-19 21:57:58 -0700

    [TRAFODION-1699] Ported the Trafodion Client Install Guide to asciidoc. Build the document using the pom.xml in docs/client_install:
       clean site post-site
    
    The build will generate the web book as well as the PDF file for the
    guide.
    
    The generated PDF file will be copied to docs/target and
    docs/target/1.3.0 as Trafodion_Client_Install_Guide.PDF.
    
    The generated web book will be copied to docs/target/client_install and
    docs/target/1.3.0/client_install. The index.html file in both these
    directories invokes the web book.
    
    Please test this level of automation. If it works, then we can make
    similar changes to other asciidoc-generated documents thereby populating
    docs/target automatically.
    
    docs/src/markdown/documentation.md has NOT been updated to point to the
    new web book. That'll be done as soon as this checkin has been verified by
    a committer.
    
    jenkins, skip test

--- win-odbc64/odbcclient/drvr35/version_drvr.cpp
+++
@@ -1,4 +0,0 @@
-// HPODBC_DRVR Version File generate, svn revision DEBUG 
-extern char* VprocString="HPODBC_DRVR Version .. Release .. (Build release [DEBUG])"; 
-extern "C" void HPODBC_DRVR_Version____Release____Build_release_DEBUG () 
-{ } 
