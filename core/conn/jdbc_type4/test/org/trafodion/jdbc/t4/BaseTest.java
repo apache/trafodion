753bf62919366008a6bd42a993fda1a8fa08595e
Kevin Xu <kaihua.xu@esgyn.cn>
2015-12-31 14:57:44 +0800

    support SQL_API_SQLSTATISTICS

--- core/conn/jdbc_type4/test/org/trafodion/jdbc/t4/BaseTest.java
+++ core/conn/jdbc_type4/test/org/trafodion/jdbc/t4/BaseTest.java
@@ -1,60 +1,63 @@
-/*
-* @@@ START COPYRIGHT @@@                                                     
-*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*
-* @@@ END COPYRIGHT @@@                                                          */
 package org.trafodion.jdbc.t4;
 
-import org.junit.After;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
+
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class BaseTest {
+	protected static String url = "jdbc:t4jdbc://10.10.10.136:23400/:";
+	protected static String driverClass = "org.trafodion.jdbc.t4.T4Driver";
+	protected static String userName = "traf123";
+	protected static String pwd = "abc";
+	protected static Connection conn;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void beforeClass() throws Exception {
+		Class.forName(driverClass);
+		conn = DriverManager.getConnection(url, userName, pwd);
+		//prepareData();
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	public static void prepareData() throws Exception {
+		Random r = new Random();
+		Statement st = conn.createStatement();
+		st.execute("create schema if not exists mylocaltest");
+		st.execute("create table if not exists mylocaltest.t1 (id int, name varchar(20),weight numeric(18,2))");
+		st.execute("insert into mylocaltest.t1 values(" + r.nextInt() + ",'aaa'," + r.nextFloat() + "),"
+				+ "(" + r.nextInt() + ",'aaa'," + r.nextFloat() + "),"
+				+ "(" + r.nextInt() + ",'aaa'," + r.nextFloat() + ")");
+		st.execute("insert into mylocaltest.t1 select * from mylocaltest.t1");
+		st.close();
+	}
+
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public static void cleanup() throws Exception {
+		try {
+			Statement st = conn.createStatement();
+			st.execute("drop schema if exists mylocaltest cascade");
+			st.close();
+		} catch (Exception e) {
+			System.err.println("Warning: " + e.getMessage());
+		}
+		if (conn != null) {
+			conn.close();
+		}
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
+	protected static void close(Statement st, ResultSet rs) throws Exception {
+		try {
+			if (rs != null)
+				rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (st != null) {
+			st.close();
+		}
 	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
-
-
 }
