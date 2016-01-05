13f897e8f6600138fd4a73594847a40faa328e40
Sandhya Sundaresan <sandhya@apache.org>
2016-01-05 21:42:39 +0000

    added copyrights to new testdriver files

--- core/sql/clitest/blobtest.h
+++ core/sql/clitest/blobtest.h
@@ -1,3 +1,37 @@
+/**********************************************************************
+// @@@ START COPYRIGHT @@@
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// @@@ END COPYRIGHT @@@
+**********************************************************************/
+/* -*-C++-*-
+****************************************************************************
+*
+* File:         Healper functions for use in bin/clitest.cpp
+* Description:  Test driver functions useing exe util cli interface
+*
+*
+*
+*
+****************************************************************************
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
