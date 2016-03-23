6f5d4cc40b2e3b40e9075cd7c916ec693b11d534
Gunnar Tapper <gtapper@esgyn.local>
2016-03-22 23:57:09 -0600

    Backup checkin. The basic edit of the guide has been done. Need to change it to refer to trafci commands to complete.

---
+++ docs/spj_guide/src/resources/source/persnl_job_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE demo.persnl.job
+( jobcode NUMERIC (4)  UNSIGNED NO DEFAULT  NOT NULL
+, jobdesc VARCHAR (18)          DEFAULT ' ' NOT NULL
+, PRIMARY KEY (jobcode)
+) ;
+
+INSERT INTO demo.persnl.job VALUES
+  ( 100, 'MANAGER' )
+, ( 200, 'PRODUCTION SUPV' )
+, ( 250, 'ASSEMBLER' )
+, ( 300, 'SALESREP' )
+, ( 400, 'SYSTEM ANALYST' )
+, ( 420, 'ENGINEER' )
+, ( 450, 'PROGRAMMER' )
+, ( 500, 'ACCOUNTANT' )
+, ( 600, 'ADMINISTRATOR' )
+, ( 900, 'SECRETARY' )
+;
+
+UPDATE STATISTICS FOR TABLE demo.persnl.job ON EVERY COLUMN ;
