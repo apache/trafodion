9f2fff6e3d7b78bb45be014992dc88ee77316680
Liang Zhang <liang.zhang@esgyn.cn>
2016-08-11 23:52:54 -0700

    make a better fix on TRAFODION-2104

--- core/conn/odb/src/odb.c
+++ core/conn/odb/src/odb.c
@@ -9889,7 +9889,7 @@ static void Oextract(int eid)
             if ( etab[eid].fho ) {
                 (*hdfswrite)(hfs, etab[eid].fho, (void *)xbuff, xbuffl);
             } else {
-                if ( etab[eid].fo == stdout ) {
+                if ( etab[eid].fo ) {
                     (void)fwrite ( xbuff, 1, xbuffl, etab[eid].fo );
                 }
             }
