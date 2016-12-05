9ee378b621de0bbb32bf2bc3987209a478d46b4d
AlexPeng19 <alex_peng1986@hotmail.com>
2016-12-05 16:15:42 +0800

    store the jdbc connection info in the properties

--- core/conn/trafci/test/main/java/org/trafodion/ci/UserInterfaceTest.java
+++ core/conn/trafci/test/main/java/org/trafodion/ci/UserInterfaceTest.java
@@ -20,29 +20,97 @@
 // @@@ END COPYRIGHT @@@
 package org.trafodion.ci;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.junit.Test;
 
 public class UserInterfaceTest {
 
 	@Test
-	public void test() {
-		MySecurityManager secManager = new MySecurityManager();
-		System.setSecurityManager(secManager);
+	public void testUserInterfaceMain() throws FileNotFoundException {
+		// MySecurityManager secManager = new MySecurityManager();
+		/*System.setSecurityManager(secManager);*/
 		try {
-			String[] params = "-h 10.10.12.99:23400 -u trafodion -p traf123 -q values(1);".split("\\s+");
-			UserInterface.main(params);
+			FileInputStream fs = new FileInputStream("src/main/resources/trafci.properties");
+			Properties prop = new Properties();
+			try {
+				prop.load(fs);
+				String host = prop.getProperty("db.host");
+				String user = prop.getProperty("db.user");
+				String pwd = prop.getProperty("db.pwd");
+				String query = prop.getProperty("db.query");
+				assertNotNull(prop);
+				assertNotNull(fs);
+				System.out.println(host + user + pwd);
+				String[] params = (host + user + pwd + query).split("\\s+");
+				System.out.println((host + user + pwd + query).split("\\s+"));
+				UserInterface.main(params);
+
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		} catch (SecurityException e) {
 			assertTrue(true);
 		}
 	}
 
-}
+	@Test
+	public void testWithRuntime() {
+		try {
+			FileInputStream fs = new FileInputStream("src/main/resources/trafci.properties");
+			Properties prop = new Properties();
+			prop.load(fs);
+			String host = prop.getProperty("db.host");
+			String user = prop.getProperty("db.user");
+			String pwd = prop.getProperty("db.pwd");
+			String query = prop.getProperty("db.query");
 
-class MySecurityManager extends SecurityManager {
-	@Override
-	public void checkExit(int status) {
-		throw new SecurityException();
+			Process p = Runtime.getRuntime().exec("trafci -h " + host + " -u " + user + " -p " + pwd + " -q " + query);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
+	@Test
+	public void testWithNewProcess() throws FileNotFoundException {
+		FileInputStream fs;
+		ProcessBuilder pb = null;
+		try {
+			fs = new FileInputStream("src/main/resources/trafci.properties");
+
+			Properties prop = new Properties();
+			prop.load(fs);
+			String host = prop.getProperty("db.host");
+			String user = prop.getProperty("db.user");
+			String pwd = prop.getProperty("db.pwd");
+			String query = prop.getProperty("db.query");
+			String output=prop.getProperty("output.file.name");
+
+			pb = new ProcessBuilder("trafci", "-h", host, "-u", user, "-p", pwd, "-q", query);
+			pb.redirectErrorStream(true);
+			File f = new File(output);
+			pb.redirectOutput(f);
+			Process p = pb.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
+/*
+ * class MySecurityManager extends SecurityManager {
+ * 
+ * @Override public void checkExit(int status) { throw new SecurityException();
+ * } }
+ */
