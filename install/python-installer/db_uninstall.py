6a65dcab0a8b045385c3147a89e873e7be6a465f
Eason <hfutmkby@gmail.com>
2017-03-07 11:21:23 +0800

    optimize ps command in uninstall script

--- install/python-installer/db_uninstall.py
+++ install/python-installer/db_uninstall.py
@@ -101,8 +101,8 @@ def main():
     # remove trafodion userid and group on all trafodion nodes, together with folders
     for remote in remotes:
         info('Remove Trafodion on node [%s] ...' % remote.host)
-        remote.execute('ps -ef|grep ^%s|awk \'{print $2}\'|xargs sudo kill -9' % TRAF_USER, chkerr=False)
-        remote.execute('ps -ef|grep krb5check|awk \'{print $2}\'|xargs sudo kill -9', chkerr=False) # traf kerberos service
+        remote.execute('ps -f -u %s|awk \'{print $2}\'|xargs sudo kill -9' % TRAF_USER, chkerr=False)
+        remote.execute('trafid=`getent passwd %s|awk -F: \'{print $3}\'`; if [[ -n $trafid ]]; then ps -f -u $trafid|awk \'{print $2}\'|xargs sudo kill -9; fi' % TRAF_USER, chkerr=False)
         remote.execute('sudo -n /usr/sbin/userdel -rf %s' % TRAF_USER, chkerr=False)
         remote.execute('sudo -n /usr/sbin/groupdel %s' % TRAF_USER, chkerr=False)
         remote.execute('sudo -n rm -rf /etc/security/limits.d/%s.conf %s /tmp/hsperfdata_%s 2>/dev/null' % (TRAF_USER, TRAF_CFG_DIR, TRAF_USER), chkerr=False)
