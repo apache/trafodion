7b2950393bd81138c85f006a0bc82b20d0de6e95
SuJinpei <873118042@qq.com>
2017-10-24 09:18:59 +0800

    original code is valid

--- core/conn/odb/src/odb.c
+++ core/conn/odb/src/odb.c
@@ -4159,7 +4159,7 @@ static void sigcatch(int sig)
         gclean();
     } else {
         for ( i = 0 ; i < tn ; i++ ) {
-            if ( pthread_kill(thid[i], 0) ) {  /* If this thread is alive... */
+            if ( !pthread_kill(thid[i], 0) ) {  /* If this thread is alive... */
                 if ( pthread_cancel(thid[i]) )  /* ... cancel it */
                     fprintf(stderr, "odb [sigcatch(%d)] - Error canceling thread %d: [%d] %s\n",
                                     __LINE__, i, errno, strerror(errno) );
