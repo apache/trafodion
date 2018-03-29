f3daa45e4bad66ebff326d34610c73a04c0cda0b
Zalo Correa <zalo.correa@esgyn.com>
2018-03-28 18:51:44 -0700

    Fixes after merge to latest in shared fork.

--- core/sqf/export/include/seabed/ms.h
+++ core/sqf/export/include/seabed/ms.h
@@ -427,6 +427,7 @@ typedef enum {
     MS_ReqType_PNodeInfo,
     MS_ReqType_ProcessInfo,
     MS_ReqType_ProcessInfoCont,
+    MS_ReqType_ProcessInfoNs,
     MS_ReqType_Set,
     MS_ReqType_Shutdown,
     MS_ReqType_ShutdownNs,
