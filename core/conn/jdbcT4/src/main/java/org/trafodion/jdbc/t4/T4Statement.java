bdbfcbef792a36659544b5b7c229d2d6aafdd36d
selvaganesang <selva@apache.org>
2019-04-04 04:43:41 +0000

    Optimizing and avoiding unnecessary function calls shown as hot spots in jprofile
    java.net.Socket.setSoTimeout
    org.trafodion.jdbc.t4.T4LoggingUtilities.log
    java.net.Socket.getLocalSocketAddress
    
    Now socket.read waits for the network timeout. It can be configured via a property networkTimeout or
    call to connection.setNetworkTimeout. If the networkTimeout is not configured, a default value of 10 secs
    is assumed.
    
    The caller is expected to set the timeout depending upon the operation being performed like loginTimeout,
    queryTimeout. Socket.read will wake up for every networkTimeout expiration and check if the timeout for the
    operation hasn't exceeded. When it exceeds, SocketTimeoutException is thrown.
    
    Changed the T4Statement and T4Restult classes to have "has a" relationship with T4Connection for
    better understanding of the code. Earlier, it had "is a" relationship with T4Connection.

--- core/conn/jdbcT4/src/main/java/org/trafodion/jdbc/t4/T4Statement.java
+++ core/conn/jdbcT4/src/main/java/org/trafodion/jdbc/t4/T4Statement.java
@@ -25,23 +25,26 @@ import java.nio.charset.CharacterCodingException;
 import java.nio.charset.UnsupportedCharsetException;
 import java.sql.SQLException;
 
-final class T4Statement extends T4Connection {
+final class T4Statement {
 	private int m_queryTimeout;
 	private String m_stmtLabel;
 	private String m_stmtExplainLabel;
 	private static short EXTERNAL_STMT = 0;
+        private InterfaceConnection m_ic; 
+	private T4Connection m_serverConnection; 
 
 	boolean m_processing = false;
 
 	// -----------------------------------------------------------------------------------
 	T4Statement(InterfaceStatement is) throws SQLException {
-		super(is.ic_);
+                m_ic = is.ic_;
+		m_serverConnection = m_ic.getT4Connection();
 		m_queryTimeout = is.queryTimeout_;
 		m_stmtLabel = is.stmtLabel_;
 		m_stmtExplainLabel = "";
 
 		if (m_stmtLabel == null) {
-			throwInternalException();
+		 	m_serverConnection.throwInternalException();
 		}
 	}// end T4Statement
 
@@ -52,9 +55,9 @@ final class T4Statement extends T4Connection {
 			String stmtLabel, int stmtLabelCharset, SQL_DataValue_def inputDataValue, SQLValueList_def inputValueList,
 			byte[] txId, boolean userBuffer) throws SQLException {
 		try {
-			getInputOutput().setTimeout(m_ic.t4props_.getNetworkTimeout());
+			m_serverConnection.getInputOutput().setTimeout(m_queryTimeout);
 
-			LogicalByteArray wbuffer = ExecuteMessage.marshal(this.m_dialogueId, sqlAsyncEnable, this.m_queryTimeout,
+			LogicalByteArray wbuffer = ExecuteMessage.marshal(m_ic.getDialogueId(), sqlAsyncEnable, this.m_queryTimeout,
 					inputRowCnt, maxRowsetSize, sqlStmtType, stmtHandle, this.EXTERNAL_STMT, sqlString,
 					sqlStringCharset, cursorName, cursorNameCharset, stmtLabel, stmtLabelCharset,
 					this.m_stmtExplainLabel, inputDataValue, inputValueList, txId, userBuffer, this.m_ic);
@@ -67,17 +70,17 @@ final class T4Statement extends T4Connection {
 		} catch (SQLException e) {
 			throw e;
 		} catch (CharacterCodingException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale,
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(),
 					"translation_of_parameter_failed", "ExecuteMessage", e.getMessage());
 			se.initCause(e);
 			throw se;
 		} catch (UnsupportedCharsetException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "unsupported_encoding", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "unsupported_encoding", e
 					.getCharsetName());
 			se.initCause(e);
 			throw se;
 		} catch (Exception e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "execute_message_error", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "execute_message_error", e
 					.getMessage());
 			se.initCause(e);
 			throw se;
@@ -91,16 +94,16 @@ final class T4Statement extends T4Connection {
 		GenericReply gr = null;
 
 		try {
-			getInputOutput().setTimeout(m_ic.t4props_.getNetworkTimeout());
-			wbuffer = GenericMessage.marshal(m_locale, messageBuffer, this.m_ic);
+			m_serverConnection.getInputOutput().setTimeout(m_queryTimeout);
+			wbuffer = GenericMessage.marshal(m_serverConnection.getLocale(), messageBuffer, this.m_ic);
 			rbuffer = getReadBuffer(executeAPI, wbuffer);
-			gr = new GenericReply(m_locale, rbuffer);
+			gr = new GenericReply(m_serverConnection.getLocale(), rbuffer);
 
 			return gr;
 		} catch (SQLException se) {
 			throw se;
 		} catch (Exception e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "execute_message_error", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "execute_message_error", e
 					.getMessage());
 
 			se.initCause(e);
@@ -116,12 +119,12 @@ final class T4Statement extends T4Connection {
 	) throws SQLException {
 
 		if (sqlString == null) {
-			throwInternalException();
+			m_serverConnection.throwInternalException();
 		}
 		try {
-			getInputOutput().setTimeout(m_ic.t4props_.getNetworkTimeout());
+			m_serverConnection.getInputOutput().setTimeout(m_queryTimeout);
 
-			LogicalByteArray wbuffer = PrepareMessage.marshal(this.m_dialogueId, sqlAsyncEnable, this.m_queryTimeout,
+			LogicalByteArray wbuffer = PrepareMessage.marshal(m_serverConnection.getDialogueId(), sqlAsyncEnable, this.m_queryTimeout,
 					stmtType, sqlStmtType, stmtLabel, stmtLabelCharset, cursorName, cursorNameCharset, moduleName,
 					moduleNameCharset, moduleTimestamp, sqlString, sqlStringCharset, stmtOptions,
 					this.m_stmtExplainLabel, maxRowsetSize, txId, this.m_ic);
@@ -134,17 +137,17 @@ final class T4Statement extends T4Connection {
 		} catch (SQLException se) {
 			throw se;
 		} catch (CharacterCodingException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale,
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(),
 					"translation_of_parameter_failed", "PrepareMessage", e.getMessage());
 			se.initCause(e);
 			throw se;
 		} catch (UnsupportedCharsetException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "unsupported_encoding", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "unsupported_encoding", e
 					.getCharsetName());
 			se.initCause(e);
 			throw se;
 		} catch (Exception e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "prepare_message_error", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "prepare_message_error", e
 					.getMessage());
 
 			se.initCause(e);
@@ -156,30 +159,30 @@ final class T4Statement extends T4Connection {
 
 	CloseReply Close() throws SQLException {
 		try {
-			LogicalByteArray wbuffer = CloseMessage.marshal(m_dialogueId, m_stmtLabel, InterfaceStatement.SQL_DROP,
+			LogicalByteArray wbuffer = CloseMessage.marshal(m_serverConnection.getDialogueId(), m_stmtLabel, InterfaceStatement.SQL_DROP,
 					this.m_ic);
 
-			getInputOutput().setTimeout(m_ic.t4props_.getNetworkTimeout());
+			m_serverConnection.getInputOutput().setTimeout(m_queryTimeout);
 
 			LogicalByteArray rbuffer = getReadBuffer(TRANSPORT.SRVR_API_SQLFREESTMT, wbuffer);
 
-			CloseReply cr = new CloseReply(rbuffer, m_ncsAddress.getIPorName(), m_ic);
+			CloseReply cr = new CloseReply(rbuffer, m_serverConnection.getNCSAddress().getIPorName(), m_ic);
 
 			return cr;
 		} catch (SQLException se) {
 			throw se;
 		} catch (CharacterCodingException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale,
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(),
 					"translation_of_parameter_failed", "CloseMessage", e.getMessage());
 			se.initCause(e);
 			throw se;
 		} catch (UnsupportedCharsetException e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "unsupported_encoding", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "unsupported_encoding", e
 					.getCharsetName());
 			se.initCause(e);
 			throw se;
 		} catch (Exception e) {
-			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_locale, "close_message_error", e
+			SQLException se = TrafT4Messages.createSQLException(m_ic.t4props_, m_serverConnection.getLocale(), "close_message_error", e
 					.getMessage());
 
 			se.initCause(e);
@@ -193,7 +196,7 @@ final class T4Statement extends T4Connection {
 
 		try {
 			m_processing = true;
-			buf = super.getReadBuffer(odbcAPI, wbuffer);
+			buf = m_serverConnection.getReadBuffer(odbcAPI, wbuffer);
 			m_processing = false;
 		} catch (SQLException se) {
 			m_processing = false;
