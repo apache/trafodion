apache/trafodion

commit eb24ed0138756d74c6acf90adc7822317501f75f

[TRAFODION-3328] Code cleanup in Trafodion
Removed the code related to process priority. Process priority is the
concept inherited frome the legacy code. It is not valid in Linux OS.

core/conn/jdbc_type2/native/ResStatisticsStatement.cpp
@@ -1482,16 +1482,15 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
                 masterStatsItems_[18].statsItem_id = SQLSTATS_NUM_CPUS;
                 masterStatsItems_[19].statsItem_id = SQLSTATS_SOURCE_STR;
                 masterStatsItems_[20].statsItem_id = SQLSTATS_SOURCE_STR_LEN;
-                masterStatsItems_[21].statsItem_id = SQLSTATS_MASTER_PRIORITY;
-                masterStatsItems_[22].statsItem_id = SQLSTATS_TRANSID;
-                masterStatsItems_[23].statsItem_id = SQLSTATS_AQR_LAST_ERROR;
-                masterStatsItems_[24].statsItem_id = SQLSTATS_AQR_NUM_RETRIES;
-                masterStatsItems_[25].statsItem_id = SQLSTATS_AQR_DELAY_BEFORE_RETRY;
-                masterStatsItems_[26].statsItem_id = SQLSTATS_ROWS_AFFECTED;
-                masterStatsItems_[27].statsItem_id = SQLSTATS_CHILD_QUERY_ID;    // new col
-                masterStatsItems_[28].statsItem_id = SQLSTATS_RECLAIM_SPACE_COUNT;
-                masterStatsItems_[29].statsItem_id = SQLSTATS_SUBQUERY_TYPE;
-                masterStatsItems_[30].statsItem_id = SQLSTATS_PARENT_QUERY_SYSTEM;
+                masterStatsItems_[21].statsItem_id = SQLSTATS_TRANSID;
+                masterStatsItems_[22].statsItem_id = SQLSTATS_AQR_LAST_ERROR;
+                masterStatsItems_[23].statsItem_id = SQLSTATS_AQR_NUM_RETRIES;
+                masterStatsItems_[24].statsItem_id = SQLSTATS_AQR_DELAY_BEFORE_RETRY;
+                masterStatsItems_[25].statsItem_id = SQLSTATS_ROWS_AFFECTED;
+                masterStatsItems_[26].statsItem_id = SQLSTATS_CHILD_QUERY_ID;    // new col
+                masterStatsItems_[27].statsItem_id = SQLSTATS_RECLAIM_SPACE_COUNT;
+                masterStatsItems_[28].statsItem_id = SQLSTATS_SUBQUERY_TYPE;
+                masterStatsItems_[29].statsItem_id = SQLSTATS_PARENT_QUERY_SYSTEM;
 
                 // MAX_MASTERSTATS_ENTRY is set to 31
                 qrid_ = masterStatsItems_[0].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
@@ -1504,25 +1503,25 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
                 bzero(parentid_, MAX_QUERY_ID_LEN+1 );
                 masterStatsItems_[16].str_max_len = MAX_QUERY_ID_LEN;
 
-                childid_ = masterStatsItems_[27].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
+                childid_ = masterStatsItems_[26].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
                 if (childid_ == NULL) { cliRC = 995; throw ("error");}
                 bzero(childid_, MAX_QUERY_ID_LEN+1);
-                masterStatsItems_[27].str_max_len = MAX_QUERY_ID_LEN;
+                masterStatsItems_[26].str_max_len = MAX_QUERY_ID_LEN;
 
                 rmsSqlSource_ = masterStatsItems_[19].str_value = new (nothrow) char[RMS_STORE_SQL_SOURCE_LEN+2];
                 if (rmsSqlSource_ == NULL) { cliRC = 996; throw ("error");}
                 bzero(rmsSqlSource_, RMS_STORE_SQL_SOURCE_LEN+2);
                 masterStatsItems_[19].str_max_len = RMS_STORE_SQL_SOURCE_LEN;
 
-                subQueryType_ = masterStatsItems_[29].str_value = new (nothrow) char[SUB_QRY_TYPE_LEN+1];
+                subQueryType_ = masterStatsItems_[28].str_value = new (nothrow) char[SUB_QRY_TYPE_LEN+1];
                 if (subQueryType_ == NULL) { cliRC = 1000; throw ("error");}
                 bzero(subQueryType_, SUB_QRY_TYPE_LEN+1);
-                masterStatsItems_[29].str_max_len = SUB_QRY_TYPE_LEN;
+                masterStatsItems_[28].str_max_len = SUB_QRY_TYPE_LEN;
 
-                parentSystem_ = masterStatsItems_[30].str_value = new (nothrow) char[PAR_SYS_NAME_LEN+1];
+                parentSystem_ = masterStatsItems_[29].str_value = new (nothrow) char[PAR_SYS_NAME_LEN+1];
                 if (parentSystem_ == NULL) { cliRC = 1001; throw ("error");}
                 bzero(parentSystem_, PAR_SYS_NAME_LEN+1);
-                masterStatsItems_[30].str_max_len = PAR_SYS_NAME_LEN;
+                masterStatsItems_[29].str_max_len = PAR_SYS_NAME_LEN;
 
                 cliRC = SQL_EXEC_GetStatisticsItems(
                     reqType,
@@ -1743,9 +1742,6 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
                     break;
                 case SQLSTATS_SOURCE_STR_LEN:
                     sqlSrcLen = masterStatsItems_[i].int64_value;
-                    break;
-                case SQLSTATS_MASTER_PRIORITY:
-                    exePriority = masterStatsItems_[i].int64_value;
                     break;
                 case SQLSTATS_TRANSID:
                     transIDnum = masterStatsItems_[i].int64_value;

core/conn/odbc/src/odbc/nsksrvrcore/ResStatisticsStatement.cpp
@@ -1440,16 +1440,15 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
 				masterStatsItems_[18].statsItem_id = SQLSTATS_NUM_CPUS;
 				masterStatsItems_[19].statsItem_id = SQLSTATS_SOURCE_STR;
 				masterStatsItems_[20].statsItem_id = SQLSTATS_SOURCE_STR_LEN;
-				masterStatsItems_[21].statsItem_id = SQLSTATS_MASTER_PRIORITY;
-				masterStatsItems_[22].statsItem_id = SQLSTATS_TRANSID;
-				masterStatsItems_[23].statsItem_id = SQLSTATS_AQR_LAST_ERROR;
-				masterStatsItems_[24].statsItem_id = SQLSTATS_AQR_NUM_RETRIES;
-				masterStatsItems_[25].statsItem_id = SQLSTATS_AQR_DELAY_BEFORE_RETRY;
-				masterStatsItems_[26].statsItem_id = SQLSTATS_ROWS_AFFECTED;
-				masterStatsItems_[27].statsItem_id = SQLSTATS_CHILD_QUERY_ID;	 // new col
-				masterStatsItems_[28].statsItem_id = SQLSTATS_RECLAIM_SPACE_COUNT;
-				masterStatsItems_[29].statsItem_id = SQLSTATS_SUBQUERY_TYPE;
-				masterStatsItems_[30].statsItem_id = SQLSTATS_PARENT_QUERY_SYSTEM;
+				masterStatsItems_[21].statsItem_id = SQLSTATS_TRANSID;
+				masterStatsItems_[22].statsItem_id = SQLSTATS_AQR_LAST_ERROR;
+				masterStatsItems_[23].statsItem_id = SQLSTATS_AQR_NUM_RETRIES;
+				masterStatsItems_[24].statsItem_id = SQLSTATS_AQR_DELAY_BEFORE_RETRY;
+				masterStatsItems_[25].statsItem_id = SQLSTATS_ROWS_AFFECTED;
+				masterStatsItems_[26].statsItem_id = SQLSTATS_CHILD_QUERY_ID;	 // new col
+				masterStatsItems_[27].statsItem_id = SQLSTATS_RECLAIM_SPACE_COUNT;
+				masterStatsItems_[28].statsItem_id = SQLSTATS_SUBQUERY_TYPE;
+				masterStatsItems_[29].statsItem_id = SQLSTATS_PARENT_QUERY_SYSTEM;
 				
 				// MAX_MASTERSTATS_ENTRY is set to 31
 				qrid_ = masterStatsItems_[0].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
@@ -1462,25 +1461,25 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
 				bzero(parentid_, MAX_QUERY_ID_LEN+1 );
 				masterStatsItems_[16].str_max_len = MAX_QUERY_ID_LEN;
 
-				childid_ = masterStatsItems_[27].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
+				childid_ = masterStatsItems_[26].str_value = new (nothrow) char[MAX_QUERY_ID_LEN+1];
 				if (childid_ == NULL) { cliRC = 995; throw ("error");}
 				bzero(childid_, MAX_QUERY_ID_LEN+1);
-				masterStatsItems_[27].str_max_len = MAX_QUERY_ID_LEN;
+				masterStatsItems_[26].str_max_len = MAX_QUERY_ID_LEN;
 
 				rmsSqlSource_ = masterStatsItems_[19].str_value = new (nothrow) char[RMS_STORE_SQL_SOURCE_LEN+2];
 				if (rmsSqlSource_ == NULL) { cliRC = 996; throw ("error");}
 				bzero(rmsSqlSource_, RMS_STORE_SQL_SOURCE_LEN+2);
 				masterStatsItems_[19].str_max_len = RMS_STORE_SQL_SOURCE_LEN;
 
-				subQueryType_ = masterStatsItems_[29].str_value = new (nothrow) char[SUB_QRY_TYPE_LEN+1];
+				subQueryType_ = masterStatsItems_[28].str_value = new (nothrow) char[SUB_QRY_TYPE_LEN+1];
 				if (subQueryType_ == NULL) { cliRC = 1000; throw ("error");}
 				bzero(subQueryType_, SUB_QRY_TYPE_LEN+1);
-				masterStatsItems_[29].str_max_len = SUB_QRY_TYPE_LEN;
+				masterStatsItems_[28].str_max_len = SUB_QRY_TYPE_LEN;
 
-				parentSystem_ = masterStatsItems_[30].str_value = new (nothrow) char[PAR_SYS_NAME_LEN+1];
+				parentSystem_ = masterStatsItems_[29].str_value = new (nothrow) char[PAR_SYS_NAME_LEN+1];
 				if (parentSystem_ == NULL) { cliRC = 1001; throw ("error");}
 				bzero(parentSystem_, PAR_SYS_NAME_LEN+1);
-				masterStatsItems_[30].str_max_len = PAR_SYS_NAME_LEN;
+				masterStatsItems_[29].str_max_len = PAR_SYS_NAME_LEN;
 
 				cliRC = SQL_EXEC_GetStatisticsItems(
 					reqType,
@@ -1701,9 +1700,6 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
 					break;
 				case SQLSTATS_SOURCE_STR_LEN:
 					sqlSrcLen = masterStatsItems_[i].int64_value;
-					break;
-				case SQLSTATS_MASTER_PRIORITY:
-					exePriority = masterStatsItems_[i].int64_value;
 					break;
 				case SQLSTATS_TRANSID:
 					transIDnum = masterStatsItems_[i].int64_value;

core/conn/odbc/src/odbc/nsksrvr/SrvrConnect.cpp
@@ -4265,23 +4265,6 @@ void __cdecl SRVR::SrvrSessionCleanup(void)
 	else
 	   t = 0;
 
-	// Fix for - The below code has been moved to before the
-	// end of session call since this was causing issues with RMS.
-	// If the service context does not have a priority set then have to
-	// default to the priority what the process was started with.
-	// We set only the master priority. SQL will adjust the compiler and
-	// ESP priorities accordingly.
-	// ++++ Note: When service-level default process priority feature is enabled
-	// ++++ then this code will not be relevant and should be removed at that time.
-	if( srvrGlobal->prtyChanged )
-	{
-	   char sqlcmd[64];
-	   sprintf(sqlcmd, "SET SESSION DEFAULT MASTER_PRIORITY '%d'",
-	           srvrGlobal->srvrPriority );
-	   EXECDIRECT(sqlcmd);
-	   srvrGlobal->prtyChanged = false;
-	}
-
 	// Fixed a problem when AutoCommit is OFF and SQL starts a transaction
 	// during session end (for e.g. dropping of any volatile tables). In this
 	// case the transaction does not get commited and new connections to the

core/sql/cli/Cli.cpp
@@ -293,36 +293,6 @@ static Lng32 SQLCLI_Prepare_Setup_Post(
 	   }
 	   masterStats->setQueryType((Int16)rootTdb->getQueryType(),
                                 rootTdb->getSubqueryType());
-	   masterStats->exePriority() = (short)
-		currContext.getCliGlobals()->myPriority();
-	      
-	   if (currContext.getSessionDefaults()->getEspPriority() > 0)
-	      masterStats->espPriority() = (short)
-		  currContext.getSessionDefaults()->getEspPriority();
-	   else if (currContext.
-		       getSessionDefaults()->getEspPriorityDelta() != 0)
-	      masterStats->espPriority() = (short)
-		  (stmtStats->getMasterStats()->exePriority() +
-		   currContext.getSessionDefaults()->getEspPriorityDelta());
-	   else 
-              masterStats->espPriority() =
-		  masterStats->exePriority();
-
-	   if (currContext.getSessionDefaults()->getMxcmpPriority() > 0)
-              masterStats->cmpPriority() = (short)
-		  currContext.getSessionDefaults()->getMxcmpPriority();
-	   else if (currContext.
-	          getSessionDefaults()->getMxcmpPriorityDelta() != 0)
-              masterStats->cmpPriority() = (short)
-		  (masterStats->exePriority() +
-		   currContext.getSessionDefaults()->getMxcmpPriorityDelta());
-	   else 
-              masterStats->cmpPriority() =
-		  masterStats->exePriority();
-
-	   masterStats->fixupPriority() = (short)
-		currContext.getSessionDefaults()->getEspFixupPriorityDelta();
-	      
 	      // detect if this query is sequential at top. If
 	      // the root doesn't have an ESP exchange as its child,
 	      // then it is sequential at top. A FirstN operator could
@@ -484,16 +454,6 @@ static Lng32 CliEpilogue(CliGlobals * cliGlobals,
 
   if (isERROR(inRetcode)) 
     {
-      // if priority of master was changed for execution, 
-      // switch it back to its original priority.
-      // Do this only for root cli level.
-      if ((numOfCliCalls == 1) &&
-	  (cliGlobals->priorityChanged()))
-	{
-	  ComRtSetProcessPriority(cliGlobals->myPriority(), FALSE);
-	  cliGlobals->setPriorityChanged(FALSE);
-	}
-
       return SQLCLI_ReturnCode(cliGlobals->currContext(),inRetcode);
     }
 
@@ -2836,17 +2796,6 @@ Lng32 SQLCLI_PerformTasks(
 	    }
 	}
 
-      // if priority of master was changed for execution and didn't get
-      // changed back to its original priority at the end of that stmt, 
-      // switch it back to its original priority now.
-      // Do this only for root cli level.
-      if ((currContext.getNumOfCliCalls() == 1) &&
-	  (cliGlobals->priorityChanged()))
-	{
-	  ComRtSetProcessPriority(cliGlobals->myPriority(), FALSE);
-	  cliGlobals->setPriorityChanged(FALSE);
-	}
-
       Lng32 retcode;
       retcode = CheckNOSQLAccessMode(*cliGlobals);
       if(isERROR(retcode)){
@@ -3079,17 +3028,7 @@ Lng32 SQLCLI_PerformTasks(
       if (isERROR(tretcode))
       {
 	return CliEpilogue(cliGlobals,statement_id,tretcode);
-    }
-
-      // if priority of master was changed for execution, 
-      // switch it back to its original priority.
-      // Do this only for root cli level.
-      if ((currContext.getNumOfCliCalls() == 1) &&
-	  (cliGlobals->priorityChanged()))
-	{
-	  ComRtSetProcessPriority(cliGlobals->myPriority(), FALSE);
-	  cliGlobals->setPriorityChanged(FALSE);
-	}
+      }
     }
 
   if (tasks & CLI_PT_SPECIAL_END_PROCESS)

core/sql/cli/ExSqlComp.cpp
@@ -100,22 +100,6 @@ inline void ExSqlComp::initRequests(Requests& req)
   req.ioStatus_ = INIT;
 }
 
-ExSqlComp::ReturnStatus ExSqlComp::changePriority(IpcPriority priority,
-						  NABoolean isDelta)
-{
-  ReturnStatus ret = SUCCESS;
-  if (! server_)
-    return ret;
-
-  short rc = server_->castToIpcGuardianServer()->changePriority(priority, isDelta);
-  if (rc != 0)
-    {
-      ret = ERROR;
-    }
-
-  return ret;
-}
-
 ExSqlComp::ReturnStatus ExSqlComp::createServer() 
 {
   ReturnStatus ret = SUCCESS;
@@ -136,27 +120,9 @@ ExSqlComp::ReturnStatus ExSqlComp::createServer()
     }
   else
     {
-      IpcPriority priority = IPC_PRIORITY_DONT_CARE;
-      
-      if (cliGlobals_->currContext()->getSessionDefaults()->getMxcmpPriority() > 0)
-	priority = cliGlobals_->currContext()->getSessionDefaults()->
-	  getMxcmpPriority();
-      else if (cliGlobals_->currContext()->getSessionDefaults()->
-	       getMxcmpPriorityDelta() != 0)
-	priority = 
-	  //	  env_->getMyProcessPriority() + 
-	  cliGlobals_->myPriority() + 
-	  cliGlobals_->currContext()->getSessionDefaults()->
-	  getMxcmpPriorityDelta();
-      if ((priority > 200) ||
-	  (priority < 1))
-	
-	priority = IPC_PRIORITY_DONT_CARE;
-	
       ComDiagsArea* diags = 0;
       if ( !( server_ = sc_->allocateServerProcess(&diags, h_,nodeName_,
-						   IPC_CPU_DONT_CARE,
-						   priority,
+                                                   IPC_CPU_DONT_CARE,
 						   1, TRUE, TRUE, 2, NULL, NULL, FALSE, NULL
 						   ) ) )
 	ret = ERROR;

core/sql/cli/ExSqlComp.h
@@ -168,8 +168,6 @@ public:
   // end connection with arkcmp. Kill it.
   void endConnection();
 
-  ReturnStatus changePriority(Lng32 IpcPriority, NABoolean isDelta);
-
   inline CollHeap * getHeap() {return h_; };
 
   NABoolean isConnected() {return (sqlcompMessage_ != NULL);};

core/sql/cli/Globals.cpp
@@ -86,10 +86,8 @@ CliGlobals::CliGlobals(NABoolean espProcess)
        totalCliCalls_(0),
        savedCompilerVersion_ (COM_VERS_COMPILER_VERSION),
        globalSbbCount_(0),
-       priorityChanged_(FALSE),
        currRootTcb_(NULL),
        processStats_(NULL),
-       savedPriority_(148), // Set it to some valid priority to start with
        tidList_(NULL),
        cliSemaphore_(NULL),
        defaultContext_(NULL),
@@ -156,8 +154,6 @@ void CliGlobals::init( NABoolean espProcess,
     ex_assert(0,errStr);
   }
 
-  ComRtGetProcessPriority(myPriority_);
-  savedPriority_ = (short)myPriority_;
   myNumCpus_ = ComRtGetCPUArray(cpuArray_, (NAHeap *)&executorMemory_);
 
   // create global structures for IPC environment
@@ -315,17 +311,6 @@ Lng32 CliGlobals::getNextUniqueContextHandle()
     contextHandle = nextUniqueContextHandle++;
     cliSemaphore_->release();
     return contextHandle;
-}
-
-IpcPriority CliGlobals::myCurrentPriority()
-{
-  IpcPriority myPriority;
-  
-  Lng32 retcode = ComRtGetProcessPriority(myPriority);
-  if (retcode)
-    return -2;
-
-  return myPriority;
 }
 
 

core/sql/cli/Globals.h
@@ -316,12 +316,6 @@ inline
   NABoolean nodeIdsAreContiguous()
            { return cpuArray_[myNumCpus_-1] == myNumCpus_ - 1; }
 
-  IpcPriority myPriority() { return myPriority_; }
-  void setMyPriority(IpcPriority p) { myPriority_= p; }
-  NABoolean priorityChanged() { return priorityChanged_;}
-  void setPriorityChanged(NABoolean v) { priorityChanged_ = v; }
-  IpcPriority myCurrentPriority();
-
   Int64 getNextUniqueNumber()
   { return ++lastUniqueNumber_; }
 
@@ -342,8 +336,6 @@ inline
   void setSavedSqlTerminateAction(short val) { savedSqlTerminateAction_ = val; }
   short getSavedSqlTerminateAction() { return savedSqlTerminateAction_; }
 
-  void setSavedPriority(short val) { savedPriority_ = val; }
-  short getSavedPriority() { return savedPriority_; }
   ExSsmpManager *getSsmpManager();
   NABoolean getIsBeingInitialized() { return inConstructor_; }
 
@@ -475,9 +467,6 @@ private:
   Lng32 myNumCpus_;
   Int32 *cpuArray_;
 
-  IpcPriority myPriority_;
-  NABoolean priorityChanged_;
-
   // timestamp when cli globals were initialized. Start of master executor.
   Int64 myStartTime_;
 
@@ -501,7 +490,6 @@ private:
   NABoolean inConstructor_; //IsExecutor should return TRUE while cliGlobals is being
                             // constructed. CliGlobals is constructed outside of CLI
                             // calls and no. of cliCalls is not yet incremented
-  short savedPriority_;
   Int32 shmId_;
   NABoolean isUncProcess_;
   char myProcessNameString_[PROCESSNAME_STRING_LEN]; // PROCESSNAME_STRING_LEN in ComRtUtils.h =40 in ms.h the equiv seabed limit is 32

commit 753fa6274abdd3b41663e876eeaaaff736959a9f

Fix for regression failure with the commit eb24ed0138756d74c6acf90adc7822317501f75f

core/conn/jdbc_type2/native/ResStatisticsStatement.cpp
@@ -1326,7 +1326,7 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
 #define MAX_ACCUMULATED_STATS_DESC  2
 #define MAX_PERTABLE_STATS_DESC     30
 
-#define MAX_MASTERSTATS_ENTRY       31
+#define MAX_MASTERSTATS_ENTRY       30
 #define MAX_MEASSTATS_ENTRY         30
 #define MAX_PERTABLE_ENTRY          10
 

core/conn/odbc/src/odbc/nsksrvrcore/ResStatisticsStatement.cpp
@@ -1284,7 +1284,7 @@ void ResStatisticsStatement::setStatistics(SRVR_STMT_HDL *pSrvrStmt, SQLSTATS_TY
 #define MAX_ACCUMULATED_STATS_DESC	2
 #define MAX_PERTABLE_STATS_DESC		30
 
-#define MAX_MASTERSTATS_ENTRY		31
+#define MAX_MASTERSTATS_ENTRY		30
 #define MAX_MEASSTATS_ENTRY		30	
 #define MAX_PERTABLE_ENTRY		10
 